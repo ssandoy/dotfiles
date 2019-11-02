31f688dafe521d13859b987df2d399a332f4fa08
Sander F. Sandøy <sanderfsandoy@hotmail.com>
2019-11-02 12:10:47 +0100

    Minor polybar-improvements

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall -9 -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar top &
-ln -s /tmp/polybar_mqueue.$! /tmp/ipc-top
+#ln -s /tmp/polybar_mqueue.$! /tmp/ipc-top
 
 polybar bottom &
 ln -s /tmp/polybar_mqueue.$! /tmp/ipc-bottom
