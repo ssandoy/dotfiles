20122631d0651491f47a8f4139f15df1529f4165
Sander F. Sandøy <sanderfsandoy@gmail.com>
2025-07-21 15:30:14 +0200

    chore/docs

--- provision.sh
+++ provision.sh
@@ -7,27 +7,25 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
-
 apt_packages=(
     git
     zsh
-    rofi
     rxvt-unicode
     stow
     htop
     curl
     vim
-    nodejs
-    npm
-    flameshot
     wget
     neofetch
-    blueman
     feh
     compton
     dunst
     yad
     slack
+    tmux
+    xclip
+    fzf
+    bat
 )
 
 snap_packages=()
@@ -36,6 +34,7 @@ snap_classic_packages=(
     code
     slack
 )
+
 echo "Checking for apt updates"
 apt update -qq
 echo "Installing apt updates"
@@ -50,61 +49,14 @@ snap install ${snap_packages[@]}
 echo "Installing snap classic base-packages"
 snap install ${snap_classic_packages[@]}  --classic
 
-## TODO ONLY DOWNLAD IF IT ISNT INSTALLED.
-if ! [ -x "$(command -v google-chrome)" ]; then
-  echo 'Chrome is not installed.' >&2
-  echo "Downlading google-chrome"
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  echo "Installing google-chrome"
-  apt install ./google-chrome-stable_current_amd64.deb
-  rm ./google-chrome-stable_current_amd64.deb
-else
-  echo 'Chrome already installed. Skipping'
-fi
-
-echo "Installing i3-gaps"
-./i3-install.sh
-echo "Installing polybar"
-./polybar-install.sh
-
-echo "Stowing config to .config"
-stow config
-
-echo "Stowing Xresources"
-stow X
-
-echo "Moving resize-font-script to .urxvt/ext"
-mkdir -p ~/.urxvt/ext
-cp X/scripts/resize-font ~/.urxvt/ext/
-xrdb -merge -I$HOME ~/.Xresources 
-
-echo "Stowing git"
-stow git
-
-echo "Stowing fonts"
-stow fonts
-
-echo "Copying background.jpg to Pictures-folder"
-cp background.jpg ~/Pictures/background.jpg
-
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &!
-
 echo "Installing oh-my-zsh-plugins"
-sleep 10
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Stowing zsh"
-rm ~/.zshrc
-stow zsh
 
 echo "Setting zsh as default shell"
 chsh -s $(which zsh)
 
-echo "Install betterlockscreen"
-./betterlockscreen/betterlockscreen.sh
 
-echo "Install nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+echo "Running stow-all.sh to symlink configs"
+./stow-all.sh
 
