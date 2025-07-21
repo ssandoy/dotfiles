b9b2a4a1941ce1c29a6bc00982f7a13b9088739b
Sander Sandøy <sanderfsandoy@gmail.com>
2025-07-21 11:50:17 +0200

    feat: v2 with slimmer setup. zsh, mise and tmux (#3)

---
+++ stow-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the absolute path to the config directory, relative to this script
+DOTFILES_DIR="$(cd "$(dirname "$0")/config" && pwd)"
+cd "$DOTFILES_DIR" || exit 1
+
+# Configs
+stow -t "$HOME" tmux
+stow -t "$HOME" zsh
+stow -t "$HOME" git
+stow -t "$HOME" vim
+stow -t "$HOME" mise
+
+# Add more as needed 
+
+#stow -t "$HOME/.config" dunst
+#stow -t "$HOME/.config" polybar
+#stow -t "$HOME/.config" rofi
+
